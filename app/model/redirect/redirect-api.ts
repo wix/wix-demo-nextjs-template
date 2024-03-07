c0513ef10254b1436511678395240e75439e5872
lior marzouk <liorm@wix.com>
2024-03-07 11:34:48 +0200

    step 4 redirect-api redirect users to wix checkout process for ecommerce flow

--- app/model/redirect/redirect-api.ts
+++ app/model/redirect/redirect-api.ts
@@ -1,9 +1,32 @@
-export const createRedirectSession = async ({}: {
+import { createClient, OAuthStrategy } from '@wix/sdk';
+import { redirects } from '@wix/redirects';
+import Cookies from 'js-cookie';
+import { WIX_REFRESH_TOKEN } from '@/app/constants';
+
+const wixClient = createClient({
+  modules: {
+    redirects,
+  },
+  auth: OAuthStrategy({
+    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+    tokens: {
+      refreshToken: JSON.parse(Cookies.get(WIX_REFRESH_TOKEN) || "{}"),
+      accessToken: { value: "", expiresAt: 0 }
+    },
+  }),
+});
+
+export const createRedirectSession = async ({callbacks, checkoutId}: {
   checkoutId: string,
   callbacks: {
     postFlowUrl: string,
     thankYouPageUrl: string,
     cartPageUrl: string,
   }}) => {
-  return { redirectSession: { fullUrl: 'http://localhost:3000/' } };
+  return wixClient.redirects.createRedirectSession({
+    ecomCheckout: {
+      checkoutId,
+    },
+    callbacks,
+  });
 }
